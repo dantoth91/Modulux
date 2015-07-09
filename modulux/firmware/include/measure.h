58ba846c513f946d93e5bfecdf58c075fe67c318
Richárd <sarkany.richard0@gmail.com>
2015-07-09 14:36:21 +0200

    Measure source created

---
+++ modulux/firmware/include/measure.h
@@ -0,0 +1,13 @@
+/*
+ * measure.h
+ *
+ *  Created on: 2015 júl. 9
+ *      Author: Richárd
+ */
+
+#ifndef INCLUDE_MEASURE_H_
+#define INCLUDE_MEASURE_H_
+
+
+
+#endif /* INCLUDE_MEASURE_H_ */
---
+++ modulux/firmware/src/measure.c
@@ -0,0 +1,13 @@
+/*
+ * measure.c
+ *
+ *  Created on: 2015 júl. 9
+ *      Author: Richárd
+ */
+
+
+#include "measure.h"
+
+#include "ch.h"
+#include "hal.h"
+#include "chprintf.h"
